7725de34463fcdf202321cecee394da7bb0039aa
Michał Mazurek <m.mazurek@bankier.pl>
2018-10-22 20:00:50 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,28 @@ var browserSync = require('browser-sync').create();
 var sourceMaps = require('gulp-sourcemaps');
 
 gulp.task('sass', () => {
-	return gulp.src('scss/**/*.scss')
-		.pipe(sourceMaps.init())
-		.pipe(sass({outputStyle:'expanded',
-		includePaths: require('node-normalize-scss').includePaths}))
-		.pipe(sourceMaps.write())
-		.pipe(gulp.dest('css'))
-		.pipe(browserSync.reload({
-      		stream: true
+  return gulp.src('scss/**/*.scss')
+    .pipe(sourceMaps.init())
+    .pipe(sass({
+      outputStyle: 'expanded',
+      includePaths: require('node-normalize-scss').includePaths
+    }))
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
     }));
 });
 
 gulp.task('browserSync', () => {
-	browserSync.init({
-		server: {
-			baseDir: './'
-		}
-	});
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
 });
 
 gulp.task('watch', ['browserSync', 'sass'], () => {
-	gulp.watch('scss/**/*.scss', ['sass'], browserSync.reload);
-	gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('scss/**/*.scss', ['sass'], browserSync.reload);
+  gulp.watch('./*.html', browserSync.reload);
 });
